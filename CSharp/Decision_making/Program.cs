9c03d06a1936d003690f22d472b458c4b25ee39d
G <hemapriya.g@dxc.com>
2023-04-20 12:39:11 +0530

    updated

---
+++ CSharp/Decision_making/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Enter three numbers");
+int num1=Convert.ToInt32(Console.ReadLine());
+int num2=Convert.ToInt32(Console.ReadLine()); 
+int num3=Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3) 
+Console.WriteLine("Num1 is Largest");
+
+else if (num2 > num1 && num2 > num3)
+    Console.WriteLine("Num2  is Largest");
+else
+    Console.WriteLine("Num3 is Largest");
---
+++ CSharp/Vowels/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the alphabet");
+String ch = Console.ReadLine();
+
+if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u" ||
+    ch == "A" || ch == "I" || ch == "O" || ch == "U" || ch == "E")
+{
+    Console.WriteLine("ch is Vowel");
+
+}
+else
+
+    Console.WriteLine("ch is not a vowel");  
+
+
