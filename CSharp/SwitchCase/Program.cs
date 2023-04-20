8f5e871ae9ee9d68e807331949b52337eb7f4913
G <hemapriya.g@dxc.com>
2023-04-20 17:23:04 +0530

    SwitchCase

---
+++ CSharp/SwitchCase/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter First Number");
+int Num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+int Num2=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the operator");
+
+Char Operator=Convert.ToChar(Console.ReadLine());
+
+int Result = 0;
+
+switch (Operator)
+{
+    case'+': 
+
+    Result = Num1 + Num2;
+    Console.WriteLine("Result is:" + Result);
+    break; 
+
+    case'-': 
+
+    Result = Num1 - Num2;
+    Console.WriteLine("Result is:" + Result);
+    break;
+
+    case '*':
+    Result = Num1 * Num2;
+    Console.WriteLine("Result is: "+Result);
+    break;
+
+    case'/':
+    Result = Num1 / Num2;
+    Console.WriteLine("Result is " + Result);
+    break;
+
+    case'%':
+    Result = Num1 % Num2;
+    Console.WriteLine("Result is:" + Result);
+    break;
+    
+    default:
+    Console.WriteLine("Enter correct input");
+    break;
+}
